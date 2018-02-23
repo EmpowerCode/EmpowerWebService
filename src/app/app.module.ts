24783697dc7756bee0a0871c18fca6b6d5564234
Gjermund Bjaanes <bjaanes@gmail.com>
2018-02-23 13:07:01 +0100

    Moving from old repo to new one

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
+import { RegisterComponent } from './register/register.component';
+import { TransferComponent } from './transfer/transfer.component';
+import { DeliverComponent } from './deliver/deliver.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: MainScreenComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'transfer', component: TransferComponent },
+  { path: 'deliver', component: DeliverComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: MainScreenComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainScreenComponent,
+    RegisterComponent,
+    TransferComponent,
+    DeliverComponent
+  ],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
